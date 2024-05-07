8a6b367ebb19f6a690b4e324306ef7464fefd7ad
danielecarpentiero <carpentierodaniele@gmail.com>
2024-05-07 19:27:40 +0200

    feat: scaffolding

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿int num1;
+int num2;
+bool exit = false;
+Console.WriteLine("Inserisci due numeri e una lettera");
+while (!exit)
+{
+    if (int.TryParse(Console.ReadLine(), out num1) && int.TryParse(Console.ReadLine(), out num2))
+    {
+        char c = char.ToUpper(Console.ReadKey().KeyChar);
+        switch (c)
+        {
+            case 'S':
+                Console.WriteLine($"la somma è {num1 + num2}");
+                break;
+            case 'M':
+                Console.WriteLine($"la moltiplicazione è {num1 * num2}");
+                break;
+            case 'X':
+                exit = true;
+                break;
+            default:
+                Console.WriteLine("Inserire S per somma, M per moltiplicare, X per uscire");
+                break;
+        }
+    }
+    else
+    {
+        char c = char.ToUpper(Console.ReadKey().KeyChar);
+        if (c == 'x')
+        {
+            exit = true;
+        }
+        else
+        {
+            Console.WriteLine("Input non valido");
+        }
+    }
+}
